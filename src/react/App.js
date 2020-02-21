caed33b9500636a728cfe9c1727f56b9e28a06a2
Braeden Orchard <braeden@restful-it-guy.services>
2020-02-21 17:36:42 +1100

    asdf

--- src/react/App.js
+++ src/react/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import './App.css';
 
-import Login from './Components/Login/Login';
-import Title from './Components/Title/Title';
-import Error from './Components/Error/Error';
-
 function App() {
   return (
     <div className="App">
-      <Title />
-      <Error errorMessage="Login" />
-      <Login />
     </div>
   );
 }
