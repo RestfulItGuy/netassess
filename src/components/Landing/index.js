0cc696de324a800e3eaec32313a8594f253ffd33
Braeden Orchard <braeden@restful-it-guy.services>
2020-02-22 15:27:12 +1100

    Title changes

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const Landing = () => (
-  <div>
-    <h1>Welcome to TICC</h1>
-  </div>
+  <>
+    <div className="titleContainer">
+      <h1 className="title">Welcome to TICC</h1>
+      <span className="subtitle">Sign in below or <a href="#">request access</a></span>
+    </div>
+  </>
 );
 
 export default Landing;
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -15,16 +15,13 @@ const NavigationAuth = () =>(
   <div>
     <ul>
       <li>
-        <Link to={ROUTES.LANDING}>Landing</Link>
-      </li>
-      <li>
         <Link to={ROUTES.HOME}>Home</Link>
       </li>
       <li>
         <Link to={ROUTES.ACCOUNT}>Account</Link>
       </li>
       <li>
-        <Link to={ROUTES.ADMIN}>Admin</Link>
+        <Link to={ROUTES.ACCOUNT}>Account</Link>
       </li>
       <li>
         <SignOutButton/>
