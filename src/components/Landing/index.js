8f111bb1fc1b3e375bfd873f34ae9ec39100f192
Braeden Orchard <braeden@restful-it-guy.services>
2020-02-22 16:11:34 +1100

    cleaned build && dist

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -4,8 +4,16 @@ const Landing = () => (
   <>
     <div className="titleContainer">
       <h1 className="title">Welcome to TICC</h1>
-      <span className="subtitle">Sign in below or <a href="#">request access</a></span>
+      {/* eslint-disable-next-line */}
+      <h3 className="subtitle">Sign in below or <a href="#">request access</a></h3>
     </div>
+    <form className="loginForm" id="loginForm">
+      <input type="text" className="textInput" id="username" placeholder="Email" />
+      <br />
+      <input type="password" className="textInput" id="password" placeholder="Password" />
+      <br />
+      <button className="submitButton">Submit</button>
+    </form>
   </>
 );
 
