b5e3a19bc782c78705717d9516d29b343868cd00
Braeden Orchard <braeden@restful-it-guy.services>
2020-02-23 12:11:50 +1100

    Restart

--- README.md
+++
@@ -1,1 +0,0 @@
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
--- electron/main.js
+++ electron/main.js
@@ -1,14 +1,19 @@
-const { app, BrowserWindow, ipcMain, ipcRenderer, Menu } = require('electron');
-const path = require('path');
-const url = require('url');
+const { app, BrowserWindow, ipcMain, protocol, Menu } = require('electron');
 const { channels } = require('../src/shared/constants');
 const { autoUpdater } = require('electron-updater');
 
+const path = require('path');
+const url = require('url');
+const log = require('electron-log');
+
+//Setup logging
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
 
 let mainWindow;
 
 function createWindow () {
-
   const startUrl = process.env.ELECTRON_START_URL || url.format({
     pathname: path.join(__dirname, '../index.html'),
     protocol: 'file:',
@@ -28,10 +33,45 @@ function createWindow () {
   mainWindow.once('ready-to-show', () => {
     autoUpdater.checkForUpdatesAndNotify();
   });
-  //Menu.setApplicationMenu(null);
+  Menu.setApplicationMenu(null);
 }
 
+function sendStatusToMainWindow(text){
+  log.info(text);
+  mainWindow.webContents.send('message', text);
+}
+
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToMainWindow('Checking for update...');
+})
+
+autoUpdater.on('update-available', (info) => {
+  sendStatusToMainWindow('Update available');
+})
+
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToMainWindow('Update not available');
+})
+
+autoUpdater.on('download-progress', (progress) => {
+  let log_message = 'Download speed: ' + progress.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progress.percent + '%';
+  log_message = log_message + '(' + progress.transferred + '/' + progress.total + ')';
+  sendStatusToMainWindow(log_message);
+})
+
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToMainWindow('Update downloaded');
+});
+
+autoUpdater.on('error', (error) => {
+  sendStatusToMainWindow('Error: ' + error);
+})
+
 app.on('ready', createWindow);
+app.on('ready', function()  {
+  autoUpdater.checkForUpdatesAndNotify();
+});
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
