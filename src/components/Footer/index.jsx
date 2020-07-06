021897cc4b60cde57bce0ec8a1632ffb9600906c
Braeden Orchard <patric2413@live.com.au>
2020-07-06 11:19:24 +1000

    Setting update message

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,32 +3,38 @@ import packageJson from '../../../package.json';
 
 const { ipcRenderer } = window.require('electron');
 let electron_message = '';
+let update_message = '';
 
 export default function Footer() {
   console.log()
   ipcRenderer.on("checking-for-update", (event, message) => {
     electron_message = message;
+    update_message = "checking-for-update";
     console.log(message)
   })
   ipcRenderer.on("update-available", (event, message) => {
     electron_message = message;
+    update_message = "update-available";
     console.log(message)
   })
   ipcRenderer.on("update-not-available", (event, message) => {
     electron_message = message;
+    update_message = "update-not-available";
     console.log(message)
   })
   ipcRenderer.on("update-downloaded", (event, message) => {
     electron_message = message;
+    update_message = "update-downloaded";
     console.log(message)
   })
   ipcRenderer.on("error", (event, message) => {
     electron_message = message;
+    update_message = "error";
     console.log(message)
   })
   return (
     <>
-      <span className="footer">{packageJson.version} | {electron_message} | C 2020</span>
+      <span className="footer">{packageJson.version} |{update_message}| {electron_message} | C 2020</span>
     </>
   )
 }
