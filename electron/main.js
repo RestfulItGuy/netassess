b8aea91614ead42d2809fe759a1de9b18ad03892
Braeden Orchard <patric2413@live.com.au>
2020-07-06 11:00:50 +1000

    Messing with update

--- electron/main.js
+++ electron/main.js
@@ -1,5 +1,4 @@
 const { app, BrowserWindow, ipcMain, protocol, Menu } = require('electron');
-// const { channels } = require('../src/shared/constants');
 const { autoUpdater } = require('electron-updater');
 
 const path = require('path');
@@ -7,11 +6,6 @@ const url = require('url');
 const log = require('electron-log');
 
 require('react-devtools-electron')
-
-//Setup logging
-autoUpdater.logger = log;
-autoUpdater.logger.transports.file.level = 'info';
-log.info('App starting...');
 
 let mainWindow;
 
@@ -22,6 +16,7 @@ function createWindow() {
     protocol: 'file:',
     slashes: true,
   });
+
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -30,85 +25,14 @@ function createWindow() {
       nodeIntegration: true
     },
   });
-  mainWindow.loadURL(startUrl);
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
 
-  const isMac = process.platform === 'darwin'
-  const template = [
-    // { role: 'appMenu' }
-    ...(isMac ? [{
-      label: app.name,
-      submenu: [
-        { role: 'about' },
-        { type: 'separator' },
-        { role: 'services' },
-        { type: 'separator' },
-        { role: 'hide' },
-        { role: 'hideothers' },
-        { role: 'unhide' },
-        { type: 'separator' },
-        { role: 'quit' }
-      ]
-    }] : []),
-    // { role: 'viewMenu' }
-    {
-      label: 'Having trouble?',
-      submenu: [
-        { role: 'forcereload' },
-        { role: 'toggledevtools' },
-      ]
-    }
-  ]
-  const menu = Menu.buildFromTemplate(template)
+  mainWindow.loadURL(startUrl);
+
+  const menu = Menu.buildFromTemplate([{ label: 'Having trouble?', submenu: [{ role: 'forcereload' }, { role: 'toggledevtools' }] }])
   Menu.setApplicationMenu(menu)
 }
 
-function sendStatusToMainWindow(text) {
-  log.info(text);
-  mainWindow.webContents.send('message', text);
-}
-
-autoUpdater.on('checking-for-update', () => {
-  sendStatusToMainWindow('checking-for-update');
-})
-
-autoUpdater.on('update-available', (info) => {
-  sendStatusToMainWindow('Update available');
-  mainWindow.webContents.send('update-available', "Update available");
-})
-
-autoUpdater.on('update-not-available', (info) => {
-  sendStatusToMainWindow('Update not available');
-  mainWindow.webContents.send('update-not-available', "Update not available");
-})
-
-autoUpdater.on('download-progress', (progress) => {
-  let log_message = 'Download speed: ' + progress.bytesPerSecond;
-  log_message = log_message + ' - Downloaded ' + progress.percent + '%';
-  log_message = log_message + '(' + progress.transferred + '/' + progress.total + ')';
-  sendStatusToMainWindow(log_message);
-  mainWindow.webContents.send('ASDF', log_message);
-})
-
-autoUpdater.on('update-downloaded', (info) => {
-  sendStatusToMainWindow('Update downloaded');
-  mainWindow.webContents.send('update-downloaded', 'Update downloaded');
-});
-
-autoUpdater.on('error', (error) => {
-  sendStatusToMainWindow('Error: ' + error);
-  mainWindow.webContents.send('error', 'Error: ' + error);
-})
-
 app.on('ready', createWindow);
-app.on('ready', function () {
-  mainWindow.webContents.on('did-finish-load', () => {
-    mainWindow.webContents.send('ASDF', 'Hello world');
-  });
-});
-
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
@@ -117,14 +41,5 @@ app.on('window-all-closed', function () {
 });
 
 app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow();
-  }
+  createWindow();
 });
-
-// ipcMain.on(channels.APP_INFO, (event) => {
-//   event.sender.send(channels.APP_INFO, {
-//     appName: app.getName(),
-//     appVersion: app.getVersion(),
-//   });
-// });
